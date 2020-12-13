aad3cec974d04c28bd661aff44abb40be25b4b0c
SanliPatras <11013825@student.unklab.ac.id>
2020-12-13 13:18:38 +0800

    last commit component

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -2,5 +2,5 @@ import React from 'react';
 import '../../App.css';
 
 export default function AboutMe() {
-  return <h1 className='about-me'> </h1>;
+  return <h1 className='about-me'>SANLI PATRAS</h1>;
 }
